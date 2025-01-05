1b8da00b35fe6f91d68f97df748c3af9e245899d
ImDhairya <pandyadhairya789@gmail.com>
2025-01-05 08:07:59 +0530

    changed a few apis

--- public/js/script.js
+++ public/js/script.js
@@ -28,7 +28,7 @@ if (navigator.geolocation) {
   });
 }
 
-const map = L.map("map").setView([0, 0], 10);
+const map = L.map("map").setView([0, 0], 16);
 
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
   attribution: "© OpenStreetMap Dhairya implementation",
@@ -38,7 +38,7 @@ const markers = {};
 
 socket.on("receive-location", (data) => {
   const {id, latitude, longitude} = data;
-  map.setView([latitude, longitude], 10);
+  map.setView([latitude, longitude], 16);
 
   if (markers[id]) {
     markers[id].setLatLng([latitude, longitude]);
